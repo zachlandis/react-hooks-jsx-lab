2569d60214667c4d9827edef96d07d01726fc8d0
Zach <zachlandis91@gmail.com>
2023-02-08 11:27:40 -0700

    commit tree f9ebf401a2f1b5547d34a12db8639dfd6099ce4c

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const style = {color: 'firebrick'}
+
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+  <div id="home">
+    <h1 style={style}>{name} is a Web Developer from {city}</h1>
+  </div>
+  )
 }
 
 export default Home;
